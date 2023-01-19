8e81a46477ec4469a781f1a2e7e4a321d0dca986
WSP99 <nosod3@naver.com>
2023-01-19 16:35:55 +0900

    [FEAT] Card Content 주입

--- app/src/main/java/com/psw9999/composesample/MainActivity.kt
+++ app/src/main/java/com/psw9999/composesample/MainActivity.kt
@@ -3,20 +3,22 @@ package com.psw9999.composesample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.psw9999.composesample.ui.theme.ComposeSampleTheme
@@ -28,6 +30,21 @@ class MainActivity : ComponentActivity() {
             ComposeSampleTheme {
                 InitScreen(modifier = Modifier.fillMaxSize())
             }
+        }
+    }
+}
+
+@Composable
+fun InitScreen(
+    modifier: Modifier = Modifier
+) {
+    val shouldShowOnBoard = rememberSaveable { mutableStateOf(true) }
+
+    Surface(modifier) {
+        if (shouldShowOnBoard.value) {
+            OnboardScreen(onContinueClicked = { shouldShowOnBoard.value = false })
+        } else {
+            CardList()
         }
     }
 }
@@ -53,64 +70,74 @@ fun OnboardScreen(
 }
 
 @Composable
-fun InitScreen(
-    modifier: Modifier = Modifier
-) {
-    val shouldShowOnBoard = rememberSaveable { mutableStateOf(true) }
-
-    Surface(modifier) {
-        if (shouldShowOnBoard.value) {
-            OnboardScreen(onContinueClicked = { shouldShowOnBoard.value = false })
-        } else {
-            MyApp()
-        }
-    }
-}
-
-
-@Composable
-fun MyApp(
+fun CardList(
     modifier: Modifier = Modifier,
     names: List<String> = List(1000) { "$it" }
 ) {
     LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
         items(items = names) { name ->
-            Greeting(name = name)
+            TestCard(name = name)
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Surface(
-        color = MaterialTheme.colorScheme.primary,
+private fun TestCard(name: String) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        ),
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        val expanded = rememberSaveable { mutableStateOf(false) }
-        val extraPadding by animateDpAsState(
-            if (expanded.value) 48.dp else 0.dp,
-            animationSpec = spring(
-                dampingRatio = Spring.DampingRatioMediumBouncy,
-                stiffness = Spring.StiffnessLow
-            )
-        )
+        CardContent(name = name)
+    }
+}
 
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
-            ) {
-                Text(text = "Hello, ")
-                Text(text = name)
-            }
-            ElevatedButton(
-                onClick = { expanded.value = !expanded.value }
-            ) {
-                Text(if (expanded.value) "Show less" else "Show more")
+@Composable
+fun CardContent(name: String) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                //.padding(bottom = extraPadding.coerceAtLeast(0.dp))
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello, ")
+            Text(
+                text = name,
+                style = MaterialTheme.typography.headlineLarge,
+                fontWeight = FontWeight.ExtraBold
+            )
+            if (expanded) {
+                Text(
+                    text = ("Compose ipsum color sit lazy, " +
+                            "padding theme elit, sed do bouncy. ").repeat(4)
+                )
             }
         }
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (expanded) {
+                    "ShowLess"
+                } else {
+                    "ShowMore"
+                }
+            )
+        }
     }
+
 }
 
 @Preview(showBackground = true, widthDp = 320)
